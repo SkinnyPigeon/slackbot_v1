8b977ccc0819453c8b0e3abc37b81a471f41676e
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-31 19:10:10 +0000

    getting a bit closer to getting the bot's id

--- client/src/app.js
+++ client/src/app.js
@@ -5,16 +5,8 @@ var NewBot = require( './models/NewBot' );
 
 var token = process.env.BOT_API_KEY;
 
-// var newBot = new NewBot({
-//     token: token
-// });
-
-// console.log( newBot );
-
-// newBot.run();
-
-var norrisbot = new NorrisBot({
+var newBot = new NewBot({
     token: token
 });
 
-norrisbot.run();
+newBot.run();
--- client/src/models/NewBot.js
+++ client/src/models/NewBot.js
@@ -1,86 +1,80 @@
 'use strict';
 
-var util = require( 'util' );
-var path = require( 'path' );
-var fs = require( 'fs' );
-var Bot = require( 'slackbots' );
- 
-var NewBot = function Constructor( settings ) {
+var util = require('util');
+var path = require('path');
+var fs = require('fs');
+var Bot = require('slackbots');
+
+var NewBot = function Constructor(settings) {
     this.settings = settings;
     this.settings.name = this.settings.name || 'NewBot';
     this.user = null;
     this.db = null;
-
-    this.run();
 };
 
-util.inherits( NewBot, Bot );
+util.inherits(NewBot, Bot);
 
-NewBot.prototype = {
+NewBot.prototype.run = function () {
+    NewBot.super_.call( this, this.settings );
 
-    // run: function() {
-    //     NewBot.super_.call( this, this.settings );
-    //     this.on( 'start', this._onStart );
-    //     this.on( 'message', this._onMessage );
-    // },
+    this.on( 'start', this._onStart );
+    this.on( 'message', this._onMessage );
+};
 
-    _onStart: function() {
-        this._loadBotUser();
-        this._welcomeMessage();
-        this.postMessageToChannel( 'general', 'Hello channel!' );
-        this.postMessageToUser( 'skinnypigeon', 'hello bro!' );
-    },
+NewBot.prototype._onStart = function () {
+    this._loadBotUser();
+    this._welcomeMessage();
+};
 
-    _loadBotUser: function() {
-        var self = this;
-        this.user = this.users.filter( function( user ) {
-            return user.name === self.name;
-        })[0];
-    },
+NewBot.prototype._loadBotUser = function () {
+    var self = this;
+    this.user = this.users.filter( function( user ) {
+        return user.name === self.name;
+    })[0];
+    console.log( this.user );
+};
 
-    _welcomeMessage: function() {
-        this.postMessageToChannel(this.channels[0].name, 'Hi guys, roundhouse-kick anyone?' +
-            '\n I can tell jokes, but very honest ones. Just say `Chuck Norris` or `' + this.name + '` to invoke me!',
-            { as_user: true });
-    },
+NewBot.prototype._welcomeMessage = function () {
+    this.postMessageToChannel( this.channels[1].name, "Hello, I am working" );
+};
 
-    _onMessage: function() {
-        console.log( message );
-        if ( this._isChatMessage( message ) &&
-             this._isChannelConversation( message ) &&
-             !this._isFromNorrisBot( message )) {
-             // this._replyWithRandomJoke( message );
-         this.sayHi( message );
-        }
-    },
-
-    sayHi: function( message ) {
-        console.log( message );
-        this.postMessageToChannel('general', 'Hello channel!');
-    },
-
-    _isChatMessage: function( message ) {
-        return message.type === 'message' && Boolean(message.text);
-    },
-
-    _isChannelConversation: function( message ) {
-        return typeof message.channel === 'string' &&
-        message.channel[0] === 'C';
-    },
-
-    _isFromNewBot: function( message ) {
-        return message.user === this.user.id;
-    },
-
-    _isMentioningNewBot: function( message ) {
-        return message.text.toLowerCase().indexOf( 'chuck norris' ) > -1 ||
-        message.text.toLowerCase().indexOf(this.name) > -1;
-    },
-
-    _replyWithRandomJoke: function( originalMessage ) {
-        this.postMessageToChannel( 'general', 'Hello channel!' );
+NewBot.prototype._onMessage = function( message ) {
+    console.log( message );
+    if ( this._isChatMessage( message ) &&
+         this._isChannelConversation( message ) &&
+         !this._isFromNewBot( message )) {
+        this._replyWithRandomJoke( message );
     }
+};
 
+NewBot.prototype.sayHi = function( message ) {
+    console.log( message );
+    this.postMessageToChannel('general', 'Hello channel!');
 }
+
+NewBot.prototype._isChatMessage = function ( message ) {
+    console.log( message );
+    return message.type === 'message' && Boolean( message.text );
+};
+
+NewBot.prototype._isChannelConversation = function( message ) {
+    return typeof message.channel === 'string';
+};
+
+NewBot.prototype._isFromNewBot = function( message ) {
+    console.log( self );
+    return message.user === this.user.id;
+};
+
+NewBot.prototype._isMentioningChuckNorris = function( message ) {
+    return message.text.toLowerCase().indexOf( 'chuck norris' ) > -1 ||
+        message.text.toLowerCase().indexOf( this.name ) > -1;
+};
+
+NewBot.prototype._replyWithRandomJoke = function( originalMessage ) {
+    this.postMessageToChannel( 'general', 'Hello channel!' );
+    this.postMessageToUser( 'skinnypigeon', 'hello bro!' );
+};
+
 
 module.exports = NewBot;
