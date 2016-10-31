0c56ca7410b5608fcb4a814cef446ef74175af53
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-31 12:44:50 +0000

    changed the login key

--- client/src/models/Bot.js
+++ client/src/models/Bot.js
@@ -2,7 +2,7 @@ var Bot = require('slackbots');
 
 // create a bot
 var settings = {
-    token: 'xoxb-98009383155-5Otl766SrDVxCnhX50UtBEOP',
+    token: 'xoxb-98009383155-yqOQlLUy7K2T8hNvdHXxK4xM',
     name: 'wedding-test-bot'
 };
 var bot = new Bot(settings);
